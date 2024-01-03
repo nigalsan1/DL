23352bedcae6b3c3d6fab8ebacb6c0d7bd3af4f9
dennisv <dennisv@student.ethz.ch>
2024-01-03 15:03:12 +0100

    Change between tennis and pong

--- decision-transformer-master/atari/run_env.py
+++ decision-transformer-master/atari/run_env.py
@@ -5,35 +5,46 @@ import numpy as np
 import cv2
 import torch
 import ale_py.roms
-# ale_py.roms.Pong = "D:/Uni/Deep_Learning/Legacy_Roms/pong.bin"
+ale_py.roms.Pong = "D:/Uni/Deep_Learning/Legacy_Roms/pong.bin"
+ale_py.roms.Tennis = "D:/Uni/Deep_Learning/Legacy_Roms/tennis.bin"
 # ale_py.roms.Pong = r"C:\Users\thiem\Downloads\atari-py-0.2.5\atari-py-0.2.5\atari_py\atari_roms\pong.bin"
-ale_py.roms.Pong = r"C:\Users\thiem\Downloads\atari-py-0.2.5\atari-py-0.2.5\atari_py\atari_roms\tennis.bin"
+# ale_py.roms.Tennis = r"C:\Users\thiem\Downloads\atari-py-0.2.5\atari-py-0.2.5\atari_py\atari_roms\tennis.bin"
+game = "Tennis"
+random_player = True
+n_steps = 100000
+context_length = 50
 
-n_steps = 1000
 
-env = gym.make("ALE/Pong-v5", render_mode="human", obs_type="grayscale", frameskip=1)
+if game == "Pong":
+    vocab_size = 6
+    target_return = 21
+elif game == "Tennis":
+    vocab_size = 18
+    target_return = 1
+
+env = gym.make(f"ALE/{game}-v5", render_mode="human", obs_type="grayscale", frameskip=1)
 observation, info = env.reset()
 
-# Load Model
-# mconf = GPTConfig(6, 150, n_layer=6, n_head=8, n_embd=128, model_type="reward_conditioned", max_timestep=4731)
-mconf = GPTConfig(18, 30, n_layer=6, n_head=8, n_embd=128, model_type="reward_conditioned", max_timestep=10460)
-model = GPT(mconf)
-# model.load_state_dict(torch.load('./saved_checkpoints/model_Pong_0.pth'))
-model.load_state_dict(torch.load('./saved_checkpoints/model_Tennis_0.pth'))
-model.eval()
+if not random_player:
+    # Load Model
+    mconf = GPTConfig(vocab_size, 150, n_layer=6, n_head=8, n_embd=128, model_type="reward_conditioned", max_timestep=4731)
+    model = GPT(mconf)
+    model.load_state_dict(torch.load(f'./saved_checkpoints/model_{game}_0.pth'))
+    model.eval()
+
 action = None
-states = np.zeros((1, 50, 4*84*84))
-actions = np.zeros((1, 50, 1))
+states = np.zeros((1, context_length, 4*84*84))
+actions = np.zeros((1, context_length, 1))
 stacked_frames = np.empty((4, 84, 84))
-rtgs = np.ones((1,50,1))
+rtgs = np.ones((1,context_length,1))
 # timesteps = torch.tensor([i for i in range(n_steps)]).resize(1,1,1)
 
-target_return = 21
+
 
 for i in range(n_steps):
 
     # Get Action every 4th frame
-    if i == 0:
+    if i == 0 or random_player:
         action = env.action_space.sample()
         # Append action to actions
         actions = np.append(actions, np.array([action]).reshape(1,1,1), axis=1)

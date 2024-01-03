d58122e934a4908dfcbcbdedb3a165399baf4baf
dennisv <dennisv@student.ethz.ch>
2024-01-03 13:05:23 +0100

    Run Environment Script

--- decision-transformer-master/atari/run_env.py
+++ decision-transformer-master/atari/run_env.py
@@ -7,54 +7,58 @@ import torch
 import ale_py.roms
 ale_py.roms.Pong = "D:/Uni/Deep_Learning/Legacy_Roms/pong.bin"
 
+n_steps = 1000
 
-
-env = gym.make("ALE/Pong-v5", render_mode="human", obs_type="grayscale")
+env = gym.make("ALE/Pong-v5", render_mode="human", obs_type="grayscale", frameskip=1)
 observation, info = env.reset()
 
 # Load Model
-mconf = GPTConfig(6, 150, n_layer=6, n_head=8, n_embd=128, model_type="reward_conditioned", max_timestep=2369)
+mconf = GPTConfig(6, 150, n_layer=6, n_head=8, n_embd=128, model_type="reward_conditioned", max_timestep=4731)
 model = GPT(mconf)
-model.load_state_dict(torch.load('./checkpoints/model.pth'))
-
+model.load_state_dict(torch.load('./saved_checkpoints/model_Pong_0.pth'))
+model.eval()
 action = None
 states = np.zeros((1, 50, 4*84*84))
 actions = np.zeros((1, 50, 1))
 stacked_frames = np.empty((4, 84, 84))
 rtgs = np.ones((1,50,1))
-# timesteps = torch.tensor([i for i in range(150)]).resize(1,1,1)
-for i in range(1000):
-    #stack first 4 frames
-    if i < 4:
+# timesteps = torch.tensor([i for i in range(n_steps)]).resize(1,1,1)
+
+target_return = 21
+
+for i in range(n_steps):
+
+    # Get Action every 4th frame
+    if i == 0:
         action = env.action_space.sample()
-        observation, reward, terminated, truncated, info = env.step(action)
-        # append observation to stacked_frames
-        stacked_frames[i] = cv2.resize(observation, (84,84))
-        continue
+        # Append action to actions
+        actions = np.append(actions, np.array([action]).reshape(1,1,1), axis=1)
+        actions = actions[:, 1:, :]
 
-    # Reshape stacked_frames
-    reshaped_frames = stacked_frames.reshape(1, 1, -1)
-    # Append reshaped_frames to states
-    states = np.append(states, reshaped_frames, axis=1)
-    states = states[:, 1:, :]
+    elif i % 4 == 0:
+        # Reshape stacked_frames
+        reshaped_frames = stacked_frames.reshape(1, 1, -1)
 
-    # states[0,i-4] = stacked_frames.flatten()
-    # states = states[1:]
+        # Append reshaped_frames to states
+        states = np.append(states, reshaped_frames, axis=1)
+        states = states[:, 1:, :]
 
-    # action = env.action_space.sample()  # agent policy that uses the observation and info
-    input = torch.from_numpy(states)
-    input = input.unsqueeze(0)
-    logits, _ = model(torch.from_numpy(states), torch.from_numpy(actions), rtgs=torch.from_numpy(rtgs), timesteps=torch.tensor([[[i]]]))
+        input = torch.from_numpy(states)
+        input = input.unsqueeze(0)
+
+        logits, _ = model(torch.from_numpy(states), torch.from_numpy(actions), rtgs=torch.from_numpy(rtgs), timesteps=torch.tensor([[[int(i/4)]]], dtype=torch.int64))
+        logits = logits[:, -1, :] # pluck the logits at the final step
+        action = int(logits.argmax(dim=-1))
+
+        # Append action to actions
+        actions = np.append(actions, np.array([action]).reshape(1,1,1), axis=1)
+        actions = actions[:, 1:, :]
+
+
+
     observation, reward, terminated, truncated, info = env.step(action)
-
-    observation = cv2.resize(observation, (84, 84)).reshape((1,84,84))
-    # remove first frame and append new frame
-    stacked_frames = stacked_frames[1:]
-    stacked_frames = np.append(stacked_frames, observation, axis=0)
-
-
-
-    if terminated or truncated:
-        observation, info = env.reset()
+    if terminated: print("terminated")
+    # append observation to stacked_frames
+    stacked_frames[i%4] = cv2.resize(observation, (84,84))
 
 env.close()
